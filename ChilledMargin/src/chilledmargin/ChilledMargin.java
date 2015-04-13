1d6af8b6ffdcb28b57a7d6e4cd2797f1305860d0
ewantland <ewantland@gmail.com>
2015-04-13 10:04:11 -0400

    FIrst attempt at commit.  Can't read text in VIM.

---
+++ ChilledMargin/src/chilledmargin/ChilledMargin.java
@@ -0,0 +1,18 @@
+// created 4/12/15 by ebw
+
+package chilledmargin;
+
+/**
+ *
+ * @author abw
+ */
+public class ChilledMargin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Evan says, \"Greetings, GitHub!\""); 
+    }
+    
+}
